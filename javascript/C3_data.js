fa09ef030ced78bc88a8aa913c370f72f1340797
ngvh2605 <48985636+ngvh2605@users.noreply.github.com>
2020-09-17 10:50:59 +1000

    Create C3_data.js

---
+++ javascript/C3_data.js
@@ -0,0 +1,97 @@
+//String colors[] = {"WHITE","SILVER", "GRAY","BLACK", "RED", "MAROON", "YELLOW","OLIVE"
+//         , "LIME", "GREEN" ,"BLUE", "PURPLE"};
+
+//String names[] = {"car","desk","refrigerator","ice","cup","chair","lamp","carpet","wardrobe","dress","bed","pillow","tool"
+//         ,"phone","earphone","mouse","keyboard","monitor","cable","glasses","calculator"};
+var item0= {name :'lamp', code :'lhq64', color :'YELLOW', price :48077, weight :24};
+var item1= {name :'cup', code :'kut74', color :'GREEN', price :32145, weight :28};
+var item2= {name :'desk', code :'hco61', color :'PURPLE', price :3752, weight :60};
+var item3= {name :'tool', code :'mea49', color :'OLIVE', price :35768, weight :14};
+var item4= {name :'lamp', code :'mpd90', color :'YELLOW', price :49978, weight :11};
+var item5= {name :'ice', code :'lfy39', color :'LIME', price :18136, weight :44};
+var item6= {name :'chair', code :'nxd09', color :'SILVER', price :44446, weight :47};
+var item7= {name :'carpet', code :'yaa54', color :'WHITE', price :45630, weight :34};
+var item8= {name :'phone', code :'kqj41', color :'BLUE', price :10436, weight :15};
+var item9= {name :'cup', code :'ixy28', color :'LIME', price :35170, weight :25};
+var item10= {name :'phone', code :'ciq65', color :'MAROON', price :3510, weight :19};
+var item11= {name :'bed', code :'zoq82', color :'RED', price :44526, weight :40};
+var item12= {name :'pillow', code :'mdj92', color :'BLACK', price :49202, weight :11};
+var item13= {name :'mouse', code :'smj65', color :'BLUE', price :33802, weight :11};
+var item14= {name :'wardrobe', code :'bvx610', color :'RED', price :5241, weight :13};
+var item15= {name :'dress', code :'thf33', color :'GRAY', price :15376, weight :56};
+var item16= {name :'car', code :'lln70', color :'BLUE', price :6616, weight :15};
+var item17= {name :'mouse', code :'jrk109', color :'LIME', price :11310, weight :53};
+var item18= {name :'ice', code :'jzj37', color :'OLIVE', price :14868, weight :59};
+var item19= {name :'carpet', code :'znv71', color :'GREEN', price :2813, weight :36};
+var item20= {name :'cup', code :'rri102', color :'YELLOW', price :13389, weight :24};
+var item21= {name :'wardrobe', code :'fht90', color :'BLUE', price :5402, weight :38};
+var item22= {name :'bed', code :'jzx72', color :'OLIVE', price :26877, weight :27};
+var item23= {name :'car', code :'qob33', color :'WHITE', price :6258, weight :31};
+var item24= {name :'dress', code :'vsa105', color :'GRAY', price :2608, weight :44};
+var item25= {name :'cup', code :'lpd56', color :'MAROON', price :43559, weight :52};
+var item26= {name :'tool', code :'rfz47', color :'OLIVE', price :19271, weight :25};
+var item27= {name :'ice', code :'ipa78', color :'SILVER', price :26724, weight :42};
+var item28= {name :'carpet', code :'gol07', color :'SILVER', price :30028, weight :39};
+var item29= {name :'wardrobe', code :'ski310', color :'GREEN', price :37650, weight :45};
+var item30= {name :'dress', code :'ftc31', color :'SILVER', price :44291, weight :42};
+var item31= {name :'carpet', code :'mae102', color :'SILVER', price :50007, weight :20};
+var item32= {name :'keyboard', code :'kzh21', color :'GRAY', price :37617, weight :23};
+var item33= {name :'mouse', code :'ghc98', color :'YELLOW', price :12366, weight :45};
+var item34= {name :'dress', code :'ear16', color :'BLUE', price :45912, weight :29};
+var item35= {name :'chair', code :'qmf82', color :'SILVER', price :21731, weight :31};
+var item36= {name :'keyboard', code :'ihd55', color :'SILVER', price :22626, weight :44};
+var item37= {name :'keyboard', code :'lpc04', color :'BLUE', price :15289, weight :17};
+var item38= {name :'dress', code :'vme46', color :'BLACK', price :46591, weight :52};
+var item39= {name :'monitor', code :'nhg510', color :'RED', price :10154, weight :60};
+var item40= {name :'mouse', code :'txv21', color :'SILVER', price :44469, weight :34};
+var item41= {name :'cup', code :'sde98', color :'BLUE', price :44703, weight :16};
+var item42= {name :'chair', code :'lrl32', color :'MAROON', price :44019, weight :33};
+var item43= {name :'car', code :'juy97', color :'SILVER', price :5711, weight :22};
+var item44= {name :'dress', code :'tzq46', color :'MAROON', price :38238, weight :36};
+var item45= {name :'refrigerator', code :'foc40', color :'PURPLE', price :10159, weight :30};
+var item46= {name :'pillow', code :'ozb71', color :'YELLOW', price :37035, weight :42};
+var item47= {name :'earphone', code :'jdh57', color :'OLIVE', price :41701, weight :58};
+var item48= {name :'refrigerator', code :'rsx108', color :'WHITE', price :30412, weight :46};
+var item49= {name :'desk', code :'rws17', color :'LIME', price :40848, weight :26};
+var item50= {name :'carpet', code :'wdl102', color :'MAROON', price :18821, weight :10};
+var item51= {name :'carpet', code :'tru14', color :'BLACK', price :36233, weight :16};
+var item52= {name :'ice', code :'lks57', color :'WHITE', price :43905, weight :11};
+var item53= {name :'pillow', code :'ryc23', color :'PURPLE', price :26380, weight :42};
+var item54= {name :'earphone', code :'bjc35', color :'BLACK', price :38362, weight :37};
+var item55= {name :'desk', code :'eig74', color :'SILVER', price :32346, weight :13};
+var item56= {name :'phone', code :'lhv93', color :'PURPLE', price :11516, weight :16};
+var item57= {name :'bed', code :'lec50', color :'MAROON', price :47660, weight :13};
+var item58= {name :'chair', code :'ovk24', color :'RED', price :47669, weight :56};
+var item59= {name :'cup', code :'cja710', color :'WHITE', price :41867, weight :40};
+var item60= {name :'chair', code :'vmb410', color :'YELLOW', price :38167, weight :11};
+var item61= {name :'keyboard', code :'nkg13', color :'OLIVE', price :32402, weight :41};
+var item62= {name :'dress', code :'roi26', color :'SILVER', price :39227, weight :58};
+var item63= {name :'earphone', code :'nzt08', color :'BLUE', price :15919, weight :35};
+var item64= {name :'earphone', code :'tzz04', color :'LIME', price :35427, weight :57};
+var item65= {name :'carpet', code :'fsa97', color :'BLUE', price :41688, weight :46};
+var item66= {name :'cable', code :'uaq61', color :'GREEN', price :3690, weight :51};
+var item67= {name :'monitor', code :'ddw01', color :'LIME', price :34004, weight :54};
+var item68= {name :'ice', code :'fvs23', color :'BLACK', price :6449, weight :36};
+var item69= {name :'phone', code :'mto18', color :'GRAY', price :5752, weight :37};
+var item70= {name :'refrigerator', code :'rlh69', color :'OLIVE', price :38371, weight :29};
+var item71= {name :'glasses', code :'qqd104', color :'WHITE', price :40224, weight :45};
+var item72= {name :'earphone', code :'nze39', color :'GRAY', price :2556, weight :18};
+var item73= {name :'glasses', code :'dlc51', color :'LIME', price :44790, weight :53};
+var item74= {name :'glasses', code :'tiz34', color :'WHITE', price :27309, weight :56};
+var item75= {name :'tool', code :'mhh45', color :'WHITE', price :49324, weight :11};
+var item76= {name :'bed', code :'gfq52', color :'BLUE', price :31391, weight :12};
+var item77= {name :'pillow', code :'vyl89', color :'OLIVE', price :43588, weight :26};
+var item78= {name :'calculator', code :'fvc05', color :'GREEN', price :8931, weight :27};
+var item79= {name :'desk', code :'lju01', color :'SILVER', price :23465, weight :19};
+
+
+
+var itemList = [item0,item1,item2,item3,item4,item5,item6,item7,item8,item9,
+    item10,item11,item12,item13,item14,item15,item16,item17,item18,item19,
+    item20,item21,item22,item23,item24,item25,item26,item27,item28,item29,
+    item30,item31,item32,item33,item34,item35,item36,item37,item38,item39,
+    item40,item41,item42,item43,item44,item45,item46,item47,item48,item49,
+    item50,item51,item52,item53,item54,item55,item56,item57,item58,item59,
+    item60,item61,item62,item63,item64,item65,item66,item67,item68,item69,
+    item70,item71,item72,item73,item74,item75,item76,item77,item78,item79,
+];
